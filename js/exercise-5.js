6cb6442ba12724064f69465b9c03af1123bd2283
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-20 12:06:03 -0800

    js for exercise-5

---
+++ js/exercise-5.js
@@ -0,0 +1,12 @@
+const studentName = "John"
+const studentGrade = 85
+
+if (studentGrade >= 80) {
+    console.log(`${studentName} got grade A`)
+} else if (studentGrade >= 60) {
+    console.log(`${studentName} got grade B`)
+} else if (studentGrade >= 40) {
+    console.log(`${studentName} got grade C`)
+} else {
+    console.log(`${studentName} got no grade`)
+}
