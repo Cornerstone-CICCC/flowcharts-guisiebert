2307e83b416e842c9bdaca70dde62f66b4a29906
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-01-20 12:00:14 -0800

    js for exercise-3

---
+++ js/exercise-3.js
@@ -0,0 +1,15 @@
+const num1 = 5
+const num2 = 10
+const num3 = 3
+
+let highest = num1
+
+if (num2 > highest) {
+    highest = num2
+}
+
+if (num3 > highest) {
+    highest = num3
+}
+
+console.log(highest)
